e274fffc37a6e621d69026321a5cd86057a64572
burbones <olia.argh@gmail.com>
2023-03-19 21:59:06 +0200

    Added tables and findGizra feature

--- src/components/grammar/GrammarCard.jsx
+++ src/components/grammar/GrammarCard.jsx
@@ -1,23 +1,15 @@
 import { Image, Box, Heading } from '@chakra-ui/react';
 import React from 'react';
-import { pics } from '../../utils/constantsGrammar';
+import { grammarTables } from '../../utils/constantsGrammar';
 
 function GrammarCard(props) {
-
-  const getTable = () => {
-    if (props.tense === "Present") {
-      return (
-        <Image w="80%" src={pics[0]} alt="table" m={5}/>
-      )
-    } 
-  }
 
   return (
     <Box align="center">
       <Heading as="h1" m={10} color="blue">
         Tense: {props.tense}, Binyan: {props.binyan}, Gizra: {props.gizra}
       </Heading>
-      {getTable()}
+      <Image w="80%" src={grammarTables[`${props.tense}_${props.binyan}_${props.gizra}`]} alt="table" m={5}/>
     </Box>
   )
 }
--- src/utils/constantsGrammar.js
+++ src/utils/constantsGrammar.js
@@ -1,5 +1,98 @@
-import PRESENT_PAAL_SHLEMIM from "../img/present-paal-shlemim.svg";
-import PRESENT_PAAL_AYN_GRONIYOT from "../img/present-paal-ayn-groniyot.svg";
+import PRESENT_PAAL_SHLEMIM_IMG from "../img/present-paal-shlemim.svg";
+import PRESENT_PAAL_AYN_GRONIYOT_IMG from "../img/present-paal-ayn-groniyot.svg";
+import PRESENT_PAAL_AYN_VAV_IUD_IMG from "../img/present-paal-ayn-vav-iud.svg";
+import PRESENT_PAAL_LAMED_HEI_IMG from "../img/present-paal-lamed-hei.svg";
+import PRESENT_PAAL_LAMED_HET_AYN_IMG from "../img/present-paal-lamed-het-ayn.svg";
+
+import PRESENT_PIEL_SHLEMIM_IMG from "../img/present-piel-shlemim.svg";
+import PRESENT_PIEL_FOUR_LETTERS_IMG from "../img/present-piel-shlemim.svg";
+import PRESENT_PIEL_AYN_GRONIYOT_IMG from "../img/present-piel-ayn-groniyot.svg";
+import PRESENT_PIEL_LAMED_HEI_IMG from "../img/present-piel-lamed-hei.svg";
+
+import PRESENT_HIFIL_SHLEMIM_IMG from "../img/present-hifil-shlemim.svg";
+import PRESENT_HIFIL_PEI_GRONIYOT_IMG from "../img/present-hifil-pei-groniyot.svg";
+import PRESENT_HIFIL_PEI_NUN_IMG from "../img/present-hifil-pei-nun.svg";
+import PRESENT_HIFIL_AYN_IUD_IMG from "../img/present-hifil-ayn-iud.svg";
+
+import PRESENT_HITPAEL_SHLEMIM_IMG from "../img/present-hitpael-shlemim.svg";
+import PRESENT_HITPAEL_AYN_GRONIYOT_IMG from "../img/present-hitpael-ayn-groniyot.svg";
+import PRESENT_HITPAEL_LAMED_HEI_IMG from "../img/present-hitpael-lamed-hei.svg";
+
+import PRESENT_NIFAL_SHLEMIM_IMG from "../img/present-nifal-shlemim.svg";
+import PRESENT_NIFAL_PEI_GRONIYOT_IMG from "../img/present-nifal-pei-groniyot.svg";
+import PRESENT_NIFAL_LAMED_HET_AYN_IMG from "../img/present-nifal-lamed-het-ayn.svg";
+
+import PAST_PAAL_SHLEMIM_IMG from "../img/past-paal-shlemim.svg";
+import PAST_PAAL_AYN_GRONIYOT_IMG from "../img/past-paal-ayn-groniyot.svg";
+import PAST_PAAL_AYN_VAV_IUD_IMG from "../img/past-paal-ayn-vav-iud.svg";
+import PAST_PAAL_LAMED_HEI_IMG from "../img/past-paal-lamed-hei.svg";
+
+import PAST_PIEL_SHLEMIM_IMG from "../img/past-piel-shlemim.svg";
+import PAST_PIEL_FOUR_LETTERS_IMG from "../img/past-piel-4letters.svg";
+import PAST_PIEL_AYN_REISH_IMG from "../img/past-piel-ayn-reish.svg";
+import PAST_PIEL_LAMED_HEI_IMG from "../img/past-piel-lamed-hei.svg";
+
+import PAST_HIFIL_SHLEMIM_IMG from "../img/past-hifil-shlemim.svg";
+import PAST_HIFIL_PEI_GRONIYOT_IMG from "../img/past-hifil-pei-groniyot.svg";
+import PAST_HIFIL_PEI_NUN_IMG from "../img/past-hifil-pei-nun.svg";
+import PAST_HIFIL_AYN_IUD_IMG from "../img/past-hifil-ayn-iud.svg";
+
+import PAST_HITPAEL_SHLEMIM_IMG from "../img/past-hitpael-shlemim.svg";
+import PAST_HITPAEL_AYN_GRONIYOT_IMG from "../img/past-hitpael-ayn-groniyot.svg";
+import PAST_HITPAEL_LAMED_HEI_IMG from "../img/past-hitpael-lamed-hei.svg";
+
+import PAST_NIFAL_SHLEMIM_IMG from "../img/past-nifal-shlemim.svg";
+import PAST_NIFAL_PEI_GRONIYOT_IMG from "../img/past-nifal-pei-groniyot.svg";
+import PAST_NIFAL_AYN_GRONIYOT_IMG from "../img/past-nifal-ayn-groniyot.svg";
+
+import FUTURE_PAAL_SHLEMIM_EFOL_IMG from "../img/future-paal-shlemim-efol.svg";
+import FUTURE_PAAL_PEI_GRONIYOT_IMG from "../img/future-paal-pei-groniyot.svg";
+import FUTURE_PAAL_PEI_IUD_IMG from "../img/future-paal-pei-iud.svg";
+import FUTURE_PAAL_SHLEMIM_EFAL_IMG from "../img/future-paal-efal.svg";
+import FUTURE_PAAL_AYN_VAV_IUD_IMG from "../img/future-paal-ayn-vav-iud.svg";
+import FUTURE_PAAL_LAMED_HEI_IMG from "../img/future-paal-lamed-hei.svg";
+
+import FUTURE_PIEL_SHLEMIM_IMG from "../img/future-piel-shlemim.svg";
+import FUTURE_PIEL_FOUR_LETTERS_IMG from "../img/future-piel-4letters.svg";
+import FUTURE_PIEL_LAMED_HEI_IMG from "../img/future-piel-lamed-hei.svg";
+
+import FUTURE_HIFIL_SHLEMIM_IMG from "../img/future-hifil-shlemim.svg";
+import FUTURE_HIFIL_PEI_GRONIYOT_IMG from "../img/future-hifil-pei-groniyot.svg";
+import FUTURE_HIFIL_PEI_NUN_IMG from "../img/future-hifil-pei-nun.svg";
+import FUTURE_HIFIL_AYN_IUD_IMG from "../img/future-hifil-ayn-iud.svg";
+
+import FUTURE_HITPAEL_SHLEMIM_IMG from "../img/future-hitpael-shlemim.svg";
+import FUTURE_HITPAEL_AYN_GRONIYOT_IMG from "../img/future-hitpael-ayn-groniyot.svg";
+import FUTURE_HITPAEL_LAMED_HEI_IMG from "../img/future-hitpael-lamed-hei.svg";
+
+import FUTURE_NIFAL_SHLEMIM_IMG from "../img/future-nifal-shlemim.svg";
+import FUTURE_NIFAL_PEI_GRONIYOT_IMG from "../img/future-nifal-pei-groniyot.svg";
+import FUTURE_NIFAL_AYN_GRONIYOT_IMG from "../img/future-nifal-ayn-groniyot.svg";
+
+import IMPERATIVE_PAAL_SHLEMIM_IMG from "../img/imperative-paal-shlemim-efol.svg";
+import IMPERATIVE_PAAL_PEI_GRONIYOT_IMG from "../img/imperative-paal-pei-groniyot.svg";
+import IMPERATIVE_PAAL_PEI_IUD_HEI_NUN_IMG from "../img/imperative-paal-pei-iud-hei-nun.svg";
+import IMPERATIVE_PAAL_AYN_GRONIYOT_IMG from "../img/imperative-paal-ayn-groniyot.svg";
+import IMPERATIVE_PAAL_AYN_VAV_IUD_IMG from "../img/imperative-paal-ayn-vav-iud.svg";
+import IMPERATIVE_PAAL_LAMED_GRONIYOT_IMG from "../img/imperative-paal-lamed-groniyot.svg";
+import IMPERATIVE_PAAL_LAMED_HEI_IMG from "../img/imperative-paal-lamed-hei.svg";
+
+import IMPERATIVE_PIEL_SHLEMIM_IMG from "../img/imperative-piel-shlemim.svg";
+import IMPERATIVE_PIEL_FOUR_LETTERS_IMG from "../img/imperative-piel-4letters.svg";
+import IMPERATIVE_PIEL_LAMED_HEI_IMG from "../img/imperative-piel-lamed-hei.svg";
+
+import IMPERATIVE_HIFIL_SHLEMIM_IMG from "../img/imperative-hifil-shlemim.svg";
+import IMPERATIVE_HIFIL_PEI_GRONIYOT_IMG from "../img/imperative-hifil-pei-groniyot.svg";
+import IMPERATIVE_HIFIL_PEI_NUN_IMG from "../img/imperative-hifil-pei-nun.svg";
+import IMPERATIVE_HIFIL_AYN_IUD_IMG from "../img/imperative-hifil-ayn-iud.svg";
+
+import IMPERATIVE_HITPAEL_SHLEMIM_IMG from "../img/imperative-hitpael-shlemim.svg";
+import IMPERATIVE_HITPAEL_AYN_GRONIYOT_IMG from "../img/imperative-hitpael-ayn-groniyot.svg";
+import IMPERATIVE_HITPAEL_LAMED_HEI_IMG from "../img/imperative-hitpael-lamed-hei.svg";
+
+import IMPERATIVE_NIFAL_SHLEMIM_IMG from "../img/imperative-nifal-shlemim.svg";
+import IMPERATIVE_NIFAL_PEI_GRONIYOT_IMG from "../img/imperative-nifal-pei-groniyot.svg";
+import IMPERATIVE_NIFAL_AYN_GRONIYOT_IMG from "../img/imperative-nifal-ayn-groniyot.svg";
 
 const SHLEMIM = "shlemim";
 const SHLEMIM_EFOL = "shlemim EF'OL";
@@ -12,9 +105,13 @@ const PEI_IUD_HEI_NUN = `פ"י, פ"ה, פ"נ`;
 const AYN_GRONIYOT = `2nd groniyot`;
 const AYN_VAV_IUD = `ע"ו, ע"י`;
 const AYN_IUD = `ע"י`;
+const AYN_REISH = `ע"ר`;
 const LAMED_HEI = `ל"ה`;
 const LAMED_HET_AYN = `ל"ח, ל"ע`;
 const LAMED_GRONIYOT = `3rd groniyot`;
+
+const GRONIYOTS = ["א", "ה", "ח", "ע"];
+const IUD_HEI_NUN = ["י", "ה", "נ"];
 
 export const menuStructure = {
     Present: {
@@ -25,29 +122,186 @@ export const menuStructure = {
         "NIF'AL": [SHLEMIM, PEI_GRONIYOT, LAMED_HET_AYN]
     },
     Past: {
-        "PA'AL": [SHLEMIM, AYN_GRONIYOT, AYN_VAV_IUD, LAMED_HEI, LAMED_HET_AYN],
-        "PI'EL": [SHLEMIM, FOUR_LETTERS, AYN_GRONIYOT, LAMED_HEI],
+        "PA'AL": [SHLEMIM, AYN_GRONIYOT, AYN_VAV_IUD, LAMED_HEI],
+        "PI'EL": [SHLEMIM, FOUR_LETTERS, AYN_REISH, LAMED_HEI],
         "HIF'IL": [SHLEMIM, PEI_GRONIYOT, PEI_NUN, AYN_IUD],
         "HITPA'EL": [SHLEMIM, AYN_GRONIYOT, LAMED_HEI],
-        "NIF'AL": [SHLEMIM, PEI_GRONIYOT, LAMED_HET_AYN]
+        "NIF'AL": [SHLEMIM, PEI_GRONIYOT, AYN_GRONIYOT]
     },
     Future: {
         "PA'AL": [SHLEMIM_EFOL, PEI_GRONIYOT, PEI_IUD, SHLEMIM_EFAL, AYN_VAV_IUD, LAMED_HEI],
         "PI'EL": [SHLEMIM, FOUR_LETTERS, LAMED_HEI],
         "HIF'IL": [SHLEMIM, PEI_GRONIYOT, PEI_NUN, AYN_IUD],
         "HITPA'EL": [SHLEMIM, AYN_GRONIYOT, LAMED_HEI],
-        "NIF'AL": [SHLEMIM, PEI_GRONIYOT, LAMED_HET_AYN]
+        "NIF'AL": [SHLEMIM, PEI_GRONIYOT, AYN_GRONIYOT]
     },
     Imperative: {
-        "PA'AL": [SHLEMIM_EFOL, PEI_GRONIYOT, PEI_IUD_HEI_NUN, PEI_GRONIYOT, AYN_VAV_IUD, LAMED_HEI, LAMED_GRONIYOT],
+        "PA'AL": [SHLEMIM, PEI_GRONIYOT, PEI_IUD_HEI_NUN, AYN_GRONIYOT, AYN_VAV_IUD, LAMED_HEI, LAMED_GRONIYOT],
         "PI'EL": [SHLEMIM, FOUR_LETTERS, LAMED_HEI],
         "HIF'IL": [SHLEMIM, PEI_GRONIYOT, PEI_NUN, AYN_IUD],
         "HITPA'EL": [SHLEMIM, AYN_GRONIYOT, LAMED_HEI],
-        "NIF'AL": [SHLEMIM, PEI_GRONIYOT, LAMED_HET_AYN]
+        "NIF'AL": [SHLEMIM, PEI_GRONIYOT, AYN_GRONIYOT]
     }
 }
 
-export const pics = [PRESENT_PAAL_SHLEMIM, PRESENT_PAAL_AYN_GRONIYOT];
+export const grammarTables = {
+    [`Present_PA'AL_${SHLEMIM}`]: PRESENT_PAAL_SHLEMIM_IMG,
+    [`Present_PA'AL_${AYN_GRONIYOT}`]: PRESENT_PAAL_AYN_GRONIYOT_IMG,
+    [`Present_PA'AL_${AYN_VAV_IUD}`]: PRESENT_PAAL_AYN_VAV_IUD_IMG,
+    [`Present_PA'AL_${LAMED_HEI}`]: PRESENT_PAAL_LAMED_HEI_IMG,
+    [`Present_PA'AL_${LAMED_HET_AYN}`]: PRESENT_PAAL_LAMED_HET_AYN_IMG,
+
+    [`Present_PI'EL_${SHLEMIM}`]: PRESENT_PIEL_SHLEMIM_IMG,
+    [`Present_PI'EL_${FOUR_LETTERS}`]: PRESENT_PIEL_FOUR_LETTERS_IMG,
+    [`Present_PI'EL_${AYN_GRONIYOT}`]: PRESENT_PIEL_AYN_GRONIYOT_IMG,
+    [`Present_PI'EL_${LAMED_HEI}`]: PRESENT_PIEL_LAMED_HEI_IMG,
+
+    [`Present_HIF'IL_${SHLEMIM}`]: PRESENT_HIFIL_SHLEMIM_IMG,
+    [`Present_HIF'IL_${PEI_GRONIYOT}`]: PRESENT_HIFIL_PEI_GRONIYOT_IMG,
+    [`Present_HIF'IL_${PEI_NUN}`]: PRESENT_HIFIL_PEI_NUN_IMG,
+    [`Present_HIF'IL_${AYN_IUD}`]: PRESENT_HIFIL_AYN_IUD_IMG,
+
+    [`Present_HITPA'EL_${SHLEMIM}`]: PRESENT_HITPAEL_SHLEMIM_IMG,
+    [`Present_HITPA'EL_${AYN_GRONIYOT}`]: PRESENT_HITPAEL_AYN_GRONIYOT_IMG,
+    [`Present_HITPA'EL_${LAMED_HEI}`]: PRESENT_HITPAEL_LAMED_HEI_IMG,
+
+    [`Present_NIF'AL_${SHLEMIM}`]: PRESENT_NIFAL_SHLEMIM_IMG,
+    [`Present_NIF'AL_${PEI_GRONIYOT}`]: PRESENT_NIFAL_PEI_GRONIYOT_IMG,
+    [`Present_NIF'AL_${LAMED_HET_AYN}`]: PRESENT_NIFAL_LAMED_HET_AYN_IMG,
+
+    [`Past_PA'AL_${SHLEMIM}`]: PAST_PAAL_SHLEMIM_IMG,
+    [`Past_PA'AL_${AYN_GRONIYOT}`]: PAST_PAAL_AYN_GRONIYOT_IMG,
+    [`Past_PA'AL_${AYN_VAV_IUD}`]: PAST_PAAL_AYN_VAV_IUD_IMG,
+    [`Past_PA'AL_${LAMED_HEI}`]: PAST_PAAL_LAMED_HEI_IMG,
+
+    [`Past_PI'EL_${SHLEMIM}`]: PAST_PIEL_SHLEMIM_IMG,
+    [`Past_PI'EL_${FOUR_LETTERS}`]: PAST_PIEL_FOUR_LETTERS_IMG,
+    [`Past_PI'EL_${AYN_REISH}`]: PAST_PIEL_AYN_REISH_IMG,
+    [`Past_PI'EL_${LAMED_HEI}`]: PAST_PIEL_LAMED_HEI_IMG,
+
+    [`Past_HIF'IL_${SHLEMIM}`]: PAST_HIFIL_SHLEMIM_IMG,
+    [`Past_HIF'IL_${PEI_GRONIYOT}`]: PAST_HIFIL_PEI_GRONIYOT_IMG,
+    [`Past_HIF'IL_${PEI_NUN}`]: PAST_HIFIL_PEI_NUN_IMG,
+    [`Past_HIF'IL_${AYN_IUD}`]: PAST_HIFIL_AYN_IUD_IMG,
+
+    [`Past_HITPA'EL_${SHLEMIM}`]: PAST_HITPAEL_SHLEMIM_IMG,
+    [`Past_HITPA'EL_${AYN_GRONIYOT}`]: PAST_HITPAEL_AYN_GRONIYOT_IMG,
+    [`Past_HITPA'EL_${LAMED_HEI}`]: PAST_HITPAEL_LAMED_HEI_IMG,
+
+    [`Past_NIF'AL_${SHLEMIM}`]: PAST_NIFAL_SHLEMIM_IMG,
+    [`Past_NIF'AL_${PEI_GRONIYOT}`]: PAST_NIFAL_PEI_GRONIYOT_IMG,
+    [`Past_NIF'AL_${AYN_GRONIYOT}`]: PAST_NIFAL_AYN_GRONIYOT_IMG,
+
+    [`Future_PA'AL_${SHLEMIM_EFOL}`]: FUTURE_PAAL_SHLEMIM_EFOL_IMG,
+    [`Future_PA'AL_${PEI_GRONIYOT}`]: FUTURE_PAAL_PEI_GRONIYOT_IMG,
+    [`Future_PA'AL_${PEI_IUD}`]: FUTURE_PAAL_PEI_IUD_IMG,
+    [`Future_PA'AL_${SHLEMIM_EFAL}`]: FUTURE_PAAL_SHLEMIM_EFAL_IMG,
+    [`Future_PA'AL_${AYN_VAV_IUD}`]: FUTURE_PAAL_AYN_VAV_IUD_IMG,
+    [`Future_PA'AL_${LAMED_HEI}`]: FUTURE_PAAL_LAMED_HEI_IMG,
+
+    [`Future_PI'EL_${SHLEMIM}`]: FUTURE_PIEL_SHLEMIM_IMG,
+    [`Future_PI'EL_${FOUR_LETTERS}`]: FUTURE_PIEL_FOUR_LETTERS_IMG,
+    [`Future_PI'EL_${LAMED_HEI}`]: FUTURE_PIEL_LAMED_HEI_IMG,
+
+    [`Future_HIF'IL_${SHLEMIM}`]: FUTURE_HIFIL_SHLEMIM_IMG,
+    [`Future_HIF'IL_${PEI_GRONIYOT}`]: FUTURE_HIFIL_PEI_GRONIYOT_IMG,
+    [`Future_HIF'IL_${PEI_NUN}`]: FUTURE_HIFIL_PEI_NUN_IMG,
+    [`Future_HIF'IL_${AYN_IUD}`]: FUTURE_HIFIL_AYN_IUD_IMG,
+
+    [`Future_HITPA'EL_${SHLEMIM}`]: FUTURE_HITPAEL_SHLEMIM_IMG,
+    [`Future_HITPA'EL_${AYN_GRONIYOT}`]: FUTURE_HITPAEL_AYN_GRONIYOT_IMG,
+    [`Future_HITPA'EL_${LAMED_HEI}`]: FUTURE_HITPAEL_LAMED_HEI_IMG,
+    
+    [`Future_NIF'AL_${SHLEMIM}`]: FUTURE_NIFAL_SHLEMIM_IMG,
+    [`Future_NIF'AL_${PEI_GRONIYOT}`]: FUTURE_NIFAL_PEI_GRONIYOT_IMG,
+    [`Future_NIF'AL_${AYN_GRONIYOT}`]: FUTURE_NIFAL_AYN_GRONIYOT_IMG,
+
+    [`Imperative_PA'AL_${SHLEMIM}`]: IMPERATIVE_PAAL_SHLEMIM_IMG,
+    [`Imperative_PA'AL_${PEI_GRONIYOT}`]: IMPERATIVE_PAAL_PEI_GRONIYOT_IMG,
+    [`Imperative_PA'AL_${PEI_IUD_HEI_NUN}`]: IMPERATIVE_PAAL_PEI_IUD_HEI_NUN_IMG,
+    [`Imperative_PA'AL_${AYN_GRONIYOT}`]: IMPERATIVE_PAAL_AYN_GRONIYOT_IMG,
+    [`Imperative_PA'AL_${AYN_VAV_IUD}`]: IMPERATIVE_PAAL_AYN_VAV_IUD_IMG,
+    [`Imperative_PA'AL_${LAMED_GRONIYOT}`]: IMPERATIVE_PAAL_LAMED_GRONIYOT_IMG,
+    [`Imperative_PA'AL_${LAMED_HEI}`]: IMPERATIVE_PAAL_LAMED_HEI_IMG,
+
+    [`Imperative_PI'EL_${SHLEMIM}`]: IMPERATIVE_PIEL_SHLEMIM_IMG,
+    [`Imperative_PI'EL_${FOUR_LETTERS}`]: IMPERATIVE_PIEL_FOUR_LETTERS_IMG,
+    [`Imperative_PI'EL_${LAMED_HEI}`]: IMPERATIVE_PIEL_LAMED_HEI_IMG,
+
+    [`Imperative_HIF'IL_${SHLEMIM}`]: IMPERATIVE_HIFIL_SHLEMIM_IMG,
+    [`Imperative_HIF'IL_${PEI_GRONIYOT}`]: IMPERATIVE_HIFIL_PEI_GRONIYOT_IMG,
+    [`Imperative_HIF'IL_${PEI_NUN}`]: IMPERATIVE_HIFIL_PEI_NUN_IMG,
+    [`Imperative_HIF'IL_${AYN_IUD}`]: IMPERATIVE_HIFIL_AYN_IUD_IMG,
+
+    [`Imperative_HITPA'EL_${SHLEMIM}`]: IMPERATIVE_HITPAEL_SHLEMIM_IMG,
+    [`Imperative_HITPA'EL_${AYN_GRONIYOT}`]: IMPERATIVE_HITPAEL_AYN_GRONIYOT_IMG,
+    [`Imperative_HITPA'EL_${LAMED_HEI}`]: IMPERATIVE_HITPAEL_LAMED_HEI_IMG,
+
+    [`Imperative_NIF'AL_${SHLEMIM}`]: IMPERATIVE_NIFAL_SHLEMIM_IMG,
+    [`Imperative_NIF'AL_${PEI_GRONIYOT}`]: IMPERATIVE_NIFAL_PEI_GRONIYOT_IMG,
+    [`Imperative_NIF'AL_${AYN_GRONIYOT}`]: IMPERATIVE_NIFAL_AYN_GRONIYOT_IMG,
+}
+
+// const testTense = "Past";
+// const testBinyan = "PA'AL";
+// const testRoot = "ע - מ - ד";
+
+export const findGizra = (tense, binyan, root) => {
+    if (root.length > 9)
+        return FOUR_LETTERS;
+
+    if (root[root.length - 1] === "ה" && (binyan === "PA'AL" || binyan === "PI'El" || binyan === "HITPA'EL"))
+        return LAMED_HEI;
+
+    if (GRONIYOTS.find((el) => el === root[0]))
+        if (menuStructure[tense][binyan].find((el) => el === PEI_GRONIYOT))
+            return PEI_GRONIYOT;
+
+    if (root[0] === "נ")
+        if (menuStructure[tense][binyan].find((el) => el === PEI_NUN))
+            return PEI_NUN;
+    
+    if (IUD_HEI_NUN.find((el) => el === root[0]) && (tense === "Imperative") && (binyan === "PA'AL"))
+        return PEI_IUD_HEI_NUN;
+        
+    if (root[0] === "י")
+        if (menuStructure[tense][binyan].find((el) => el === PEI_IUD))
+            return PEI_IUD;
+
+    if (GRONIYOTS.find((el) => el === root[4])) {
+        if (tense === "Future" && binyan === "PA'AL")
+            return SHLEMIM_EFAL;
+        else if (menuStructure[tense][binyan].find((el) => el === AYN_GRONIYOT))
+            return AYN_GRONIYOT;
+    }
+
+    if (root[4] === "י" && binyan === "HIF'IL")
+        return AYN_IUD;
+    
+    if (root[4] === "י" || root[4] === "י")
+        if (menuStructure[tense][binyan].find((el) => el === AYN_VAV_IUD))
+            return AYN_VAV_IUD;
+    
+    if (root[4] === "ר" && tense === "Past" && binyan === "PI'EL")
+        return AYN_REISH;
+
+    if (GRONIYOTS.find((el) => el === root[root.length-1]) && (tense === "Future") && (binyan === "PA'AL"))
+        return SHLEMIM_EFAL;
+    
+    if ((root[root.length-1] === "ח" || root[root.length-1] === "ע") && tense === "Present" && ((binyan === "PA'AL") || (binyan === "NIF'AL")))
+        return LAMED_HET_AYN;
+        
+    if (GRONIYOTS.find((el) => el === root[root.length-1]))
+        if (menuStructure[tense][binyan].find((el) => el === LAMED_GRONIYOT))
+            return LAMED_GRONIYOT;
+
+    if (tense === "Future" && binyan === "PA'AL")
+        return SHLEMIM_EFOL;
+
+    return SHLEMIM;
+} 
+
+// console.log(findGizra(testTense, testBinyan, testRoot));
 
 // export const tensesForms = {'Past': PAST,'Present':PRESENT, 'Future':FUTURE, 'Imperative':IMPERATIVE}
 // export const tenses = ['Past','Present', 'Future', 'Imperative']
