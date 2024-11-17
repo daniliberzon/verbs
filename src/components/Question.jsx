a59d990aeadddb01947f3e196253f502391ac601
olga.liberzon <olia.argh@gmail.com>
2024-11-17 11:17:25 +0200

    Added quiz no-mouse interaction

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -15,21 +15,32 @@ function Question(props) {
         props.setAnswer(e.target.value)
     }
 
+    function handleKeyDown(e) {
+      if (e.key === 'Enter') {
+        if (e.target.className === 'option'){
+          props.setChosen(e.target.id)
+        }
+        if (e.target.className === 'submitButton'){
+          props.setMode(2)
+        }
+      }
+    }
+
   return (
-    <div onClick={handleClick}>
+    <div onClick={handleClick} onKeyDown={handleKeyDown}>
       <p className='verb'>{props.data[props.rightAnswer][31]}</p>
         <div className='questionOptions'>
             <div className='question'>Choose translation:</div>
             {props.options.map((x) =>{
                 let className = x==props.chosen ? 'option chosen' : 'option'
                 return(
-                    <div className={className}  id={x} key={x}>{props.data[x][3]}</div>
+                    <div className={className} tabIndex="0" id={x} key={x}>{props.data[x][3]}</div>
                 )
             })}
         </div>
         <div className='question'>Type form: {decodeForm(props.columns[props.form][1])}</div>
         <Input w="50%" fontSize="20px" borderColor="black" id='answerInput' dir='rtl' onChange={handleChange} value={props.answer} autoComplete='off' />
-        <div className='submitButton'>Submit</div>
+        <div className='submitButton' tabIndex="0">Submit</div>
     </div>
   )
 }
